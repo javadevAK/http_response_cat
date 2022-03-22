44c02565b83426327660e68fa2e914deae42347b
javadevAK <java.devlearn@gmail.com>
2022-03-22 20:25:33 +0300

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,44 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class App {
+
+    private final static String URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+
+        //создаем http-client
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        //объект запроса
+        HttpGet request = new HttpGet(URI);
+
+        //отправка запроса
+        CloseableHttpResponse response = httpClient.execute(request);
+
+//        String body = new String(response.getEntity().getContent().readAllBytes());
+//        System.out.println(body);
+        List<Cat> cats = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {});
+
+        cats
+                .stream().filter(value -> value.getUpvotes() != null && Integer.parseInt(value.getUpvotes()) > 0)
+                .forEach(System.out::println);
+
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,78 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cat {
+
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private String upvotes;
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setUpvotes(String upvotes) {
+        this.upvotes = upvotes;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getUpvotes() {
+        return upvotes;
+    }
+
+
+    public Cat(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") String upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes='" + upvotes + '\'' +
+                '}';
+    }
+}
